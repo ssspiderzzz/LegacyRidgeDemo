e0f12cc173b27c7280ddff05e4f8eda3b2db90dc
Haibin Zhu <ssspiderzzz@gmail.com>
2021-01-31 20:26:33 -0800

    add text in gallary

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -4,7 +4,16 @@ const Gallary = (props) => {
   return (
     <>
       <div className="py-3 w-full h-screen">
-        <div className="w-full h-screen bg-gallary-background"></div>
+        <div className="w-full h-screen bg-gallary-background">
+          <div className="w-80% m-auto py-10 text-left text-white text-3xl tracking-widest flex flex-wrap">
+            <div className="py-12 mr-12 h-40 font-extrabold flex-1 flex items-center">
+              <span>39 SPECTACULAR VIEW LOTS</span>
+            </div>
+            <div className="py-12 mr-12 h-40 font-light flex-1 flex items-center">
+              <span>NOW SELLING!</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
